06eaaf24e50ded931a313a6a6582f8649bb28b9a
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-07 09:54:03 -0800

    lat long

--- backend/migrations/03_reports.js
+++ backend/migrations/03_reports.js
@@ -6,9 +6,10 @@ exports.up = function(knex) {
   return knex.schema.createTable('reports', function (table) {
     table.increments('report_id');
     table.string('time');
-    table.string('location');
     table.string('frequency_band');    
     table.string('mission');   
+    table.integer('latitude');   
+    table.integer('longitude');  
     table.integer('satellite_id').foreign('satellites.id');  // check satellite table
     table.integer('user_id').foreign('users.userId'); // check user table
   })
