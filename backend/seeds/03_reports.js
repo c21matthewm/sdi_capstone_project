5b9d36789661bf794a504836cc776ddbc4775e06
c21matthewm <matthewmark111@gmail.com>
2023-12-07 12:22:24 -0700

    fixed incrementing IDs on seed files

--- backend/seeds/03_reports.js
+++ backend/seeds/03_reports.js
@@ -6,9 +6,9 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('reports').del()
   await knex('reports').insert([
-    {reportID: 1, time: '2022-12-17T03:24:00', latitude: 30, longitude: 40, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 2},
-    {reportID: 2, time: '2022-12-15T03:14:00', latitude: 40, longitude: 30, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 3},
-    {reportID: 3, time: '2022-12-12T02:24:00', latitude: 50, longitude: 20, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 4}
+    {time: '2022-12-17T03:24:00', latitude: 30, longitude: 40, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 2},
+    {time: '2022-12-15T03:14:00', latitude: 40, longitude: 30, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 3},
+    {time: '2022-12-12T02:24:00', latitude: 50, longitude: 20, frequency_band: "UHF", mission: "mission_name", satelliteID: 1, userID: 4}
   ]);
 };
 
