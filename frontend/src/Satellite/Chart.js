c4a66dfb60c872c08474ed3b572327295e2aca75
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-12 14:58:37 -0800

    need to fix chart

--- frontend/src/Satellite/Chart.js
+++ frontend/src/Satellite/Chart.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
+import { userContext } from '../App';
 
 export function Chart() {
   const location = useLocation();
   const { sat } = location.state;
-  const [reports, setReports] = useState([])
+  const { reports } = useContext(userContext)
   const [jan, setJan] = useState(0)
   const [feb, setFeb] = useState(0)
   const [mar, setMar] = useState(0)
@@ -19,10 +20,7 @@ export function Chart() {
   const [oct, setOct] = useState(0)
   const [nov, setNov] = useState(0)
   const [dec, setDec] = useState(0)
-  // let dec=0;
-  // let nov=0;
-  // let oct=0;
-
+  // const [currentSat, setSat] = useState(sat.satelliteID)
   useEffect(() => {
     let xjan = 0;
     let xfeb = 0;
@@ -37,67 +35,64 @@ export function Chart() {
     let xnov = 0;
     let xdec = 0;
 
-    fetch(`http://localhost:8080/reports/satellites/${sat.satelliteID}`)
-      .then((res) => res.json())
-      .then((data) => { setReports(data) })
-      .then(() => {
-        reports.map((date) => {
-          let time = new Date(date.time);
-          let month = time.getMonth();
-          if (month === 0)
-            xjan++;
-          else if (month === 1)
-            xfeb++;
-          else if (month === 2)
-            xmar++;
-          else if (month === 3)
-            xapr++;
-          else if (month === 4)
-            xmay++;
-          else if (month === 5)
-            xjun++;
-          else if (month === 6)
-            xjul++;
-          else if (month === 7)
-            xaug++;
-          else if (month === 8)
-            xsep++;
-          else if (month === 9)
-            xoct++;
-          else if (month === 10)
-            xnov++;
-          else if (month === 11)
-            xdec++;
-        })
-      }).then(() => {
-        setDec(xdec);
-        setNov(xnov);
-        setOct(xoct);
-        setSep(xsep);
-        setAug(xaug);
-        setJul(xjul);
-        setJun(xjun);
-        setMay(xmay);
-        setApr(xapr);
-        setMar(xmar);
-        setFeb(xfeb);
-        setJan(xjan);
-      })
-  }, [])
+    reports.filter((report) => (report.satelliteID === sat.satelliteID))
+      .map((report) => {
+        let date = new Date(report.time);
+        let month = date.getMonth();
+        if (month === 0)
+          xjan++;
+        else if (month === 1)
+          xfeb++;
+        else if (month === 2)
+          xmar++;
+        else if (month === 3)
+          xapr++;
+        else if (month === 4)
+          xmay++;
+        else if (month === 5)
+          xjun++;
+        else if (month === 6)
+          xjul++;
+        else if (month === 7)
+          xaug++;
+        else if (month === 8)
+          xsep++;
+        else if (month === 9)
+          xoct++;
+        else if (month === 10)
+          xnov++;
+        else if (month === 11)
+          xdec++;
+  })
+  // .then(() => {
+  //   setDec(xdec);
+  //   setNov(xnov);
+  //   setOct(xoct);
+  //   setSep(xsep);
+  //   setAug(xaug);
+  //   setJul(xjul);
+  //   setJun(xjun);
+  //   setMay(xmay);
+  //   setApr(xapr);
+  //   setMar(xmar);
+  //   setFeb(xfeb);
+  //   setJan(xjan);
+  // })
+}, []);
 
-  return (
-    <>
-      <LineChart
-        xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }]}
-        series={[
-          {
-            curve: "linear",
-            data: [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec],
-          },
-        ]}
-        width={500}
-        height={300}
-      />
-    </>
-  );
+return (
+  <>
+    <LineChart
+      xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }]}
+      series={[
+        {
+          curve: "linear",
+          data: [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec],
+        },
+      ]}
+      width={500}
+      height={300}
+    />
+  </>
+);
 }
