dbb54478699bde53f9892589aeac9bc2916ae3d6
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-15 12:45:27 -0800

    border

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -53,8 +53,10 @@ export const Dashboard = () => {
 						{satellites.filter((satellite) => satellite.favorites.includes(userUID)).map((sat, index) => {
 							return (
 								<div id={index}className="tile">
-									<Box className="box" component="section" sx={{ boxShadow: 3, p: 2, border: '1px solid grey' }} style={{backgroundColor: sat.status === 'GREEN' ? "rgb(60, 179, 113, .8)" :
-											sat.status === 'YELLOW' ? "#facb6c" : "rgb(255, 0, 0, .8)"}}
+									<Box className="box" component="section" sx={{ boxShadow: 3, p: 2, border: '1px solid grey', borderRadius:"10px"}} 
+									// style={{backgroundColor: sat.status === 'GREEN' ? "rgb(50, 200, 70, .8)" :
+									// 		sat.status === 'YELLOW' ? "#facb6c" : "rgb(255, 0, 0, .8)"
+									// 	}}
 									variant="outlined">
 										<CardActionArea >
 											<Link to={`/satellites/${sat.satelliteID}`} state={{ sat }}>
@@ -66,6 +68,9 @@ export const Dashboard = () => {
 														width: '200px',
 														margin: '20px 0 0 0' ,
 														}}
+														style={{border: sat.status === 'GREEN' ? "10px solid rgb(0, 200, 0)" :
+											sat.status === 'YELLOW' ? "10px solid rgb(255, 255, 100)" : "10px solid rgb(255, 30, 10)"
+										}}
 														src={sat.image}
 														alt="satellite image"
 													/>
