c20b63d598c80758e5b6adb17dd1fb4cb1f7b18e
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-19 12:43:20 -0800

    style

--- frontend/src/NavBar/NavBar.js
+++ frontend/src/NavBar/NavBar.js
@@ -12,71 +12,70 @@ import { ListItemButton } from '@mui/material';
 
 export const NavBar = () => {
 
-    const userSignOut = () => {
-        signOut(auth)
-            .then(() => {
-                console.log('sign out succesful')
-            })
-            .catch((error) => console.log(error))
-    }
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('sign out succesful')
+      })
+      .catch((error) => console.log(error))
+  }
 
-    return (
-        <AppBar sx={{ background: "#4169E1" }} position="static">
-            <div className="left-align">
-                <Toolbar disableGutters={true} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'space-between' } }}>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        SatBridge
-                    </Typography>
-                    <Box sx={{ justifyContent: 'flex-start' }}>
+  return (
+    <AppBar sx={{ background: "#4169E1" }} position="static">
+      <div className="left-align">
+        <Toolbar disableGutters={true} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'space-between' } }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            SatBridge
+          </Typography>
+          <Box sx={{ justifyContent: 'flex-start' }}>
 
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            <ListItemButton component={Link} to={`/`} >
-                                <ListItemText primary={`DASHBOARD`} />
-                            </ListItemButton>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <ListItemButton component={Link} to={`/`} >
+                <ListItemText primary={`DASHBOARD`} />
+              </ListItemButton>
 
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                <ListItemButton component={Link} to={`/satellites`} >
-                                    <ListItemText primary={`SATELLITE LIST`} />
-                                </ListItemButton>
-                            </Box>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                <ListItemButton component={Link} to={`/reports`} >
-                                    <ListItemText primary={`REPORTS`} />
-                                </ListItemButton>
-                            </Box>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                <ListItemButton component={Link} to={`/addreport`} >
-                                    <ListItemText primary={`SUBMIT REPORT`} />
-                                </ListItemButton>
-                            </Box>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                <ListItemButton component={Link} to={`/map`} >
-                                    <ListItemText primary={`MAP`} />
-                                </ListItemButton>
-                            </Box>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                <ListItemButton component={Link} to={`/`} onClick={userSignOut} >
-                                    <ListItemText primary={`SIGN OUT`} />
-                                </ListItemButton>
-                            </Box>
-
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </div>
-        </AppBar>
-    );
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <ListItemButton component={Link} to={`/satellites`} >
+                  <ListItemText primary={`SATELLITE LIST`} />
+                </ListItemButton>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <ListItemButton component={Link} to={`/reports`} >
+                  <ListItemText primary={`REPORTS`} />
+                </ListItemButton>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <ListItemButton component={Link} to={`/addreport`} >
+                  <ListItemText primary={`SUBMIT REPORT`} />
+                </ListItemButton>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <ListItemButton component={Link} to={`/map`} >
+                  <ListItemText primary={`MAP`} />
+                </ListItemButton>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <ListItemButton component={Link} to={`/`} onClick={userSignOut} >
+                  <ListItemText primary={`SIGN OUT`} />
+                </ListItemButton>
+              </Box>
+            </Box>
+          </Box>
+        </Toolbar>
+      </div>
+    </AppBar>
+  );
 };
