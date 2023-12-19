54a4c2be693e9c93ec2bfa93153884efe8c4f003
ella <96279971+hiaytus@users.noreply.github.com>
2023-12-19 10:56:30 -0800

    bar chart

--- frontend/src/ReportList/BarChart.js
+++ frontend/src/ReportList/BarChart.js
@@ -10,8 +10,22 @@ const chartSetting = {
       label: 'error type',
     },
   ],
-  width: 500,
-  height: 300,
+  xAxes: [
+    {
+    ticks: {
+          autoSkip: false,
+          maxRotation: 90,
+          minRotation: 90
+      },
+      // tickLabelStyle: {
+      //   angle: 45,
+      //   textAnchor: 'start',
+      //   fontSize: 12,
+      // }
+    }
+  ],
+  // width: 500,
+  // height: 300,
   // sx: {
   //   [`.${axisClasses.left} .${axisClasses.label}`]: {
   //     transform: 'translate(-20px, 0)',
@@ -23,37 +37,15 @@ export default function BarsDataset() {
   const { reports, satellites } = useContext(userContext);
   const [reload , setReload] = useState(null)
   const [dataset, setDataset] = useState([{
-    unknownData: 3,
-    losData: 7,
-    atmoData: 6,
-    interferenceData: 1,
+    unknownData: 0,
+    losData: 0,
+    atmoData: 0,
+    interferenceData: 0,
     latencyData: 0,
     malfunctionData: 0,
     powerData: 0,
     coordinationData: 0,
-    satellite: 'Jan'
-  },
-  {
-    unknownData: 5,
-    losData: 7,
-    atmoData: 8,
-    interferenceData: 1,
-    latencyData: 0,
-    malfunctionData: 0,
-    powerData: 0,
-    coordinationData: 0,
-    satellite: 'June'
-  },
-  {
-    unknownData: 5,
-    losData: 5,
-    atmoData: 8,
-    interferenceData: 1,
-    latencyData: 0,
-    malfunctionData: 0,
-    powerData: 0,
-    coordinationData: 0,
-    satellite: 'July'
+    satellite: 'sat'
   }])
 
   //   useEffect(() => {
@@ -84,6 +76,7 @@ export default function BarsDataset() {
       let powerData = 0;
       let coordinationData = 0;
       let satname;
+      let flag = false;
       fetch(`http://localhost:8080/satellites/${sat.satelliteID}/reports`)
         .then(res => {
           if (res.status !== 200)
@@ -97,27 +90,35 @@ export default function BarsDataset() {
                     switch (string) {
                       case 'Unknown Issue':
                         unknownData += 1;
+                        flag = true;
                         break;
                       case 'Blocked LOS':
                         losData += 1;
+                        flag = true;
                         break;
                       case 'Atmospheric Conditions':
                         atmoData += 1;
+                        flag = true;
                         break;
                       case 'Signal Interference':
                         interferenceData += 1;
+                        flag = true;
                         break;
                       case 'Signal Latency':
                         latencyData += 1;
+                        flag = true;
                         break;
                       case 'Equipment Malfunction':
                         malfunctionData += 1;
+                        flag = true;
                         break;
                       case 'Power Supply Issue':
                         powerData += 1;
+                        flag = true;
                         break;
                       case 'Frequency Coordination':
                         coordinationData += 1;
+                        flag = true;
                         break;
                       default:
                         console.log('none');
@@ -127,7 +128,7 @@ export default function BarsDataset() {
                 }
               })
               .then(() => {
-                if (satname) {
+                if (flag) {
                   temp.push({
                     unknownData: unknownData,
                     losData: losData,
@@ -154,15 +155,25 @@ export default function BarsDataset() {
       dataset={dataset}
       xAxis={[{ scaleType: 'band', dataKey: 'satellite' }]}
       series={[
-        { dataKey: 'unknownData', label: 'unknownData', stack: "total" },
-        { dataKey: 'losData', label: 'losData', stack: "total" },
-        { dataKey: 'interferenceData', label: 'interferenceData', stack: "total" },
-        { dataKey: 'latencyData', label: 'latencyData', stack: "total" },
-        { dataKey: 'malfunctionData', label: 'malfunctionData', stack: "total" },
-        { dataKey: 'powerData', label: 'powerData', stack: "total" },
-        { dataKey: 'coordinationData', label: 'coordinationData', stack: "total" },
+        { dataKey: 'unknownData', label: 'Unknown', stack: "total" },
+        { dataKey: 'losData', label: 'LOS', stack: "total" },
+        { dataKey: 'interferenceData', label: 'Inteference', stack: "total" },
+        { dataKey: 'latencyData', label: 'Latency', stack: "total" },
+        { dataKey: 'malfunctionData', label: 'Malfunction', stack: "total" },
+        { dataKey: 'powerData', label: 'Power', stack: "total" },
+        { dataKey: 'coordinationData', label: 'Coordination', stack: "total" },
 
       ]}
+      slotProps={{
+        legend: {
+          direction: "column",
+          position:  {vertical: 'top', horizontal: 'right'},
+          margin: 20,
+          labelStyle: {
+            fontsize: 10,
+          },
+        }
+      }}
       {...chartSetting}
     />
   );
